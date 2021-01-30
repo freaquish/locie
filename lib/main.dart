7e602cf71d4cbd271932bdfd9d50abdb97be94e9
Piyush Jaiswal <iampiyushjaiswal103@gmail.com>
2021-01-30 12:59:29 +0530

    registration Screen ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:locie/pages/authentication.dart';
+import 'package:locie/views/registration_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -31,7 +32,7 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: AuthenticationWidget(),
+      body: RegistrationScreen(),
     );
   }
 }
