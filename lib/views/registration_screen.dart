602eaf23ca29c0eb8260939467480b5f0c1934a4
Piyush Jaiswal <iampiyushjaiswal103@gmail.com>
2021-01-30 12:46:53 +0530

    General yaml setup

---
+++ lib/views/registration_screen.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:locie/components/flatActionButton.dart';
+import 'package:locie/components/primary_container.dart';
+import 'package:locie/components/text_field.dart';
