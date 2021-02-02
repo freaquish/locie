d1852700e148bb74d8a96ad25b70ca4470fb3ca6
suyashmadhesia <suyashmadhesia@gmail.com>
2021-02-03 00:50:17 +0530

    splash screen ui

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:locie/views/Store_view/product.dart';
 import 'package:locie/views/Store_view/store_view.dart';
 import 'package:locie/views/not_internet_widget.dart';
+import 'package:locie/views/splash_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -40,9 +41,10 @@ class _MainPageState extends State<MainPage> {
               child: CircularProgressIndicator(),
             );
           } else {
-            return StoreViewWidget(
-              sid: "abaf3de1-1558-5960-9dd5-e7c04eb54767",
-            );
+            return SplashScreen();
+            // return StoreViewWidget(
+            //   sid: "abaf3de1-1558-5960-9dd5-e7c04eb54767",
+            // );
           }
         },
       ),
